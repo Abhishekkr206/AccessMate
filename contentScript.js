782c6c90c9c33524258b20266409bad171ad83f6
Abhishekkr206 <abhishek.krr206@gmail.com>
2025-07-20 22:13:25 +0530

    fix the reference error

--- contentScript.js
+++ contentScript.js
@@ -7,8 +7,8 @@ document.body.prepend(h1); // Add to top of the page
 
 console.log("Content script loaded successfully!");
 
-chrome.runtime.onMessage.addListner(function (req, sender, sendRes) {
-  if (request.action === "zoomText") {
+chrome.runtime.onMessage.addListener(function (req, sender, sendRes) {
+  if (req.action === "zoomText") {
     document.body.style.fontSize = "larger"
   }
 })
