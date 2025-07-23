93700a08485af2f05415db3e7ab6ccef0f43c8fe
Aman Singh <amanaspirantsingh@gmail.com>
2025-07-23 12:16:34 +0530

    Implement contrast mode selection and update popup functionality

--- popup/popup.js
+++ popup/popup.js
@@ -1,14 +1,28 @@
 console.log("working");
 
-document.addEventListener("DOMContentLoaded", function () {
-  const contrastBtn = document.getElementById("contrastBtn");
+document.addEventListener("DOMContentLoaded", () => {
+  const dropdown = document.getElementById("contrastMode");
 
-  if (contrastBtn) {
-    contrastBtn.addEventListener("click", () => {
-      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        chrome.tabs.sendMessage(tabs[0].id, { action: "toggleContrast" });
-      });
-    });
-  }
+  //storing it to chrome storage
+  chrome.storage.sync.get("contrastMode", (data) => {
+    if (data.contrastMode) {
+      dropdown.value = data.contrastMode;
+    }
+  });
+  
+  dropdown.addEventListener("change", () => {
+    const selectMode = dropdown.value;
+
+    // one more sync
+    chrome.storage.sync.set({ contrastMode: selectMode });
+
+    //now in contentScript
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.tabs.sendMessage(tabs[0].id, {
+        action: "applyContrastMode",
+        mode: selectMode
+      })
+    })
+  })
 });
 
